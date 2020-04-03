248198653bd658acbe5659113be2b31bb41d03b4
Haruaki OTAKE <aaharu@hotmail.com>
2020-04-03 12:53:36 +0900

    feat!: drop sizes and return sizes

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ module.exports = function (contentBuffer) {
 
   // user options
   var config = loaderUtils.getOptions(this);
-
-  config.base64 = "base64" in config ? config.base64 : true;
-  config.palette = "palette" in config ? config.palette : false;
+  var forceJimp = "forceJimp" in config ? !!config.forceJimp : false;
+  var width = "width" in config ? config.width : 20;
 
   var contentIsUrlExport = /^(module.exports =|export default) "data:(.*)base64,(.*)/.test(
     content
@@ -33,48 +32,24 @@ module.exports = function (contentBuffer) {
     source = content.match(/^(module.exports =|export default) (.*);/)[2];
   }
 
-  // promise array in case users want both
-  // base64 & color palettes generated
-  // that means we have 2 promises to resolve
-  var outputPromises = [];
-
-  if (config.base64 === true) {
-    outputPromises.push(lqip.base64(path));
-  } else {
-    // push null if the user did not wish to use Base64 to preserve the order
-    outputPromises.push(null);
-  }
-
-  // color palette generation is set to false by default
-  // since it is little bit slower than base64 generation
-  // if users wants it, grab it!
-
-  if (config.palette === true) {
-    outputPromises.push(lqip.palette(path));
-  } else {
-    // push null if the user did not wish to use palette to preserve the order
-    outputPromises.push(null);
-  }
-
-  // final step, resolving all the promises we got so far
-  Promise.all(outputPromises)
-    .then((data) => {
-      if (data) {
-        var result = 'module.exports = { "src": ' + source;
-        // either null or base64
-        if (data[0]) {
-          result += ', "preSrc": ' + JSON.stringify(data[0]);
-        }
-        // either null or palette
-        if (data[1]) {
-          result += ', "palette": ' + JSON.stringify(data[1]);
-        }
-        result += " };";
-        // the output will be sent to webpack!
-        callback(null, result);
-      } else {
-        callback(new Error("No data received"), null);
-      }
+  lqip(path, { forceJimp, width })
+    .then((result) => {
+      var sizes = {
+        originalWidth: result.metadata.originalWidth,
+        originalHeight: result.metadata.originalHeight,
+        width: result.metadata.width,
+        height: result.metadata.height,
+      };
+      var result =
+        'module.exports = { "src": ' +
+        source +
+        ', "preSrc": ' +
+        JSON.stringify(result.metadata.dataURIBase64) +
+        ', "sizes": ' +
+        JSON.stringify(sizes) +
+        " };";
+      // the output will be sent to webpack!
+      callback(null, result);
     })
     .catch((error) => {
       console.error(error);
